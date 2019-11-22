e6c1d4101adaf94534e500ec2a0ea72be9bcc9af
tuyaqi12306 <1055126409@qq.com>
2019-11-22 22:44:56 +0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Header from './common/header'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route} from 'react-router-dom'
 import store from './store'
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header/>
+        <div>
+          <Header/>
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact></Route>
+              <Route path='/detail'></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
