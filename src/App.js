58179f0e6899a369fa5231fccaad117f1688405a
tuyaqi12306 <1055126409@qq.com>
2019-11-28 12:13:35 +0800

    write article

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter, Route} from 'react-router-dom'
 import Home from './pages/home'
 import Detail from './pages/detail'
+import Write from './pages/write'
 import Login from './pages/login'
 import store from './store'
 
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path='/' exact component={Home}></Route>
             <Route path='/login' exact component={Login}></Route>
             <Route path='/detail/:id' component={Detail}></Route>
+            <Route path='/write' component={Write}></Route>
           </div>
         </BrowserRouter>
       </Provider>
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -102,10 +102,12 @@ class Header extends React.Component{
           </SearchWrapper>
         </Nav>
         <Addition>
-          <Button className='writting'>
-            <i className='iconfont'>&#xe603;</i>
-            写文章
-          </Button>
+          <Link to='/write'>
+            <Button className='writting'>
+              <i className='iconfont'>&#xe603;</i>
+              写文章
+            </Button>
+          </Link>
           <Button className='reg'>注册</Button>
         </Addition>
       </HeaderWrapper>
